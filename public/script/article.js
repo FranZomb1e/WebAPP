852d279334c72ce026424bb45937b483fe11d7ac
FranZomb1e <predator.brick@gmail.com>
2018-04-06 19:53:36 -0400

    upload

--- public/script/article.js
+++ public/script/article.js
@@ -1,4 +1,5 @@
 document.getElementById("Reddit").addEventListener("click",function(){
+  document.getElementById("u1").innerHTML = "";
   fetch("/links/reddit").then(
     function(response){
       if(response.status !== 200){
@@ -12,7 +13,7 @@ document.getElementById("Reddit").addEventListener("click",function(){
           var anchor = document.createElement('a')
           li.appendChild(anchor)
           anchor.href =  link.url
-          anchor.innerText = link.titile
+          anchor.innerText = link.title
           article.appendChild(li)
         })
         
@@ -23,27 +24,77 @@ document.getElementById("Reddit").addEventListener("click",function(){
 });
 
 
-// document.getElementById("HackerNews").addEventListener("click",function(){
-//   fetch("/links/hacknews").then(
-//     function(response){
-//       if(response.status !== 200){
-//         console.err(err)
-//         return
-//       }
-//       response.json().then(function(result) {
-//         const article = document.querySelector('#article ul')
-//         result.forEach((link) => {
-//           var li = document.createElement('li')
-//           var anchor = document.createElement('a')
-//           li.appendChild(anchor)
-//           anchor.href =  link.url
-//           anchor.innerText = link.title
-//           article.appendChild(li)
-//         })
+document.getElementById("HackerNews").addEventListener("click",function(){
+  document.getElementById("u1").innerHTML = "";
+  fetch("/links/hackernews").then(
+    function(response){
+      if(response.status !== 200){
+        console.err(err)
+        return
+      }
+      response.json().then(function(result) {
+        const article = document.querySelector('#article ul')
+        result.forEach((link) => {
+          var li = document.createElement('li')
+          var anchor = document.createElement('a')
+          li.appendChild(anchor)
+          anchor.href =  link.url
+          anchor.innerText = link.title
+          article.appendChild(li)
+        })
         
-//       })
+      })
       
-//     }
-//   )
-// });
+    }
+  )
+});
 
+document.getElementById("Medium").addEventListener("click",function(){
+  document.getElementById("u1").innerHTML = "";
+  fetch("/links/medium").then(
+    function(response){
+      if(response.status !== 200){
+        console.err(err)
+        return
+      }
+      response.json().then(function(result) {
+        const article = document.querySelector('#article ul')
+        result.forEach((link) => {
+          var li = document.createElement('li')
+          var anchor = document.createElement('a')
+          li.appendChild(anchor)
+          anchor.href =  link.url
+          anchor.innerText = link.title
+          article.appendChild(li)
+        })
+        
+      })
+      
+    }
+  )
+});
+
+document.getElementById("Slashdot").addEventListener("click",function(){
+  document.getElementById("u1").innerHTML = "";
+  fetch("/links/slashdot").then(
+    function(response){
+      if(response.status !== 200){
+        console.err(err)
+        return
+      }
+      response.json().then(function(result) {
+        const article = document.querySelector('#article ul')
+        result.forEach((link) => {
+          var li = document.createElement('li')
+          var anchor = document.createElement('a')
+          li.appendChild(anchor)
+          anchor.href =  link.url
+          anchor.innerText = link.title
+          article.appendChild(li)
+        })
+        
+      })
+      
+    }
+  )
+});
